b2e72e131edf68183f7270377a8912bb178b2089
Efe Can KIRBIYIK <kirbiyike22@itu.edu.tr>
2025-12-22 17:03:40 +0300

    train config

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -2,9 +2,11 @@
 
 
 export PYTHONPATH=./
-GPUS=8
+GPUS=1
 
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	--dist configs/svg/svg_pointT.yaml  \
 	--exp_name baseline_nclsw_grelu \
-	--sync_bn
+	--sync_bn \
+	--work_dir ./work_dirs/deneme \
+	--resume ./configs/svg/best.pth \
