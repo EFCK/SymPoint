5513caf8ce135e364ae47019dadad35abaf34e7e
EFCK <kirbiyike22@itu.edu.tr>
2025-12-23 18:20:25 +0000

    Colab update

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -7,6 +7,6 @@ GPUS=1
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	configs/svg/svg_pointT.yaml \
   --dist   \
-	--exp_name sympointv1_deneme \
+	--exp_name sympointb1_batchsize1_lr00005_poilabs_T4 \
 	--work_dir /temp/work_dir/ \
 	--resume /content/drive/MyDrive/My_Computer/sympoint_data/svg/best.pth \
