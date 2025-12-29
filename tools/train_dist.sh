843f81611e939eb99fa646c14e54aea8d1507da2
EFCK <kirbiyike22@itu.edu.tr>
2025-12-29 15:07:58 +0000

    Colab update

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -6,7 +6,7 @@ GPUS=1
 
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	configs/svg/svg_pointT.yaml \
-  --dist   \
-	--exp_name sympointb1_batchsize1_lr00005_poilabs_T4 \
-	--work_dir /temp/work_dir/ \
+  --dist \
+	--exp_name lr00008_freeze3_fpc \
+	--work_dir /temp/work_dir/lr00008_freeze3_fpc/ \
 	--resume /content/drive/MyDrive/My_Computer/sympoint_data/svg/best.pth \
