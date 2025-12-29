fd6818660dc37227392b4ec29953221707c2d318
Efe Can KIRBIYIK <kirbiyike22@itu.edu.tr>
2025-12-29 18:09:17 +0300

    Update infer_dist.sh

--- tools/infer_dist.sh
+++ tools/infer_dist.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=./
 GPUS=1
 workdir=.
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/inference.py \
-	$workdir/configs/svg/svg_pointT.yaml  $workdir/configs/svg/best.pth --out ./results/manuel_split --datadir dataset/json/manuel_split
+	$workdir/configs/svg/svg_pointT.yaml  $workdir/configs/svg/best.pth --out ./results/demo_with_curves/processed --datadir dataset/json/demov2_processed
