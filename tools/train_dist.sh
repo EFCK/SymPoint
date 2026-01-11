fd48a9464c8241c2a4738adba6a8944246ba335a
EFCK <kirbiyike22@itu.edu.tr>
2026-01-11 19:13:33 +0000

    Colab update

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -7,6 +7,6 @@ GPUS=1
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	configs/svg/svg_pointT.yaml \
   --dist \
-	--exp_name lr00008_freeze5_fp2_dropout01 \
-	--work_dir /content/drive/MyDrive/My_Computer/sympoint_data/lr00008_freeze5_fp2_dropout01/ \
+	--exp_name lr00008_freeze5_fp2_dropout01_bh_r10 \
+	--work_dir /content/drive/MyDrive/My_Computer/sympoint_data/lr00008_freeze5_fp2_dropout01_bh_r10/ \
 	--resume /content/drive/MyDrive/My_Computer/sympoint_data/svg/best.pth \
