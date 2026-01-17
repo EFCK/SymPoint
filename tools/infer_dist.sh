35881c90075d90a78df28c63a60e7f33ab2b5fc3
Efe Can KIRBIYIK <kirbiyike22@itu.edu.tr>
2026-01-17 20:12:58 +0300

    finetune results added

--- tools/infer_dist.sh
+++ tools/infer_dist.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=./
 GPUS=1
 workdir=.
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/inference.py \
-	$workdir/configs/svg/svg_pointT.yaml  $workdir/configs/svg/best.pth --out ./results/griding_test/griding_test_manuel --datadir dataset/json/griding_test_manuel
+	$workdir/configs/svg/svg_pointT.yaml  $workdir/configs/lr00008_freeze5_fp2_dropout01_bh_r10/best.pth --out ./results/manuel_split/lr00008_freeze5_fp2_dropout01_bh_r10/ --datadir dataset/json/manuel_split_fp2_test/
